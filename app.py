47fb6c9334b0d822f76aad7b6156f3133168a701
Rhiannon Schofield <yenschofield@gmail.com>
2023-03-15 13:01:30 +0000

    add example and working dropdown

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+import json
+from flask import Flask, render_template, send_file
+
+
 app = Flask(__name__)
+
+
 @app.route("/")
 def index():
-  # Your code for fetching the data goes here
-    data={{user:'Alex', PetsName:'Fish'}, {user:'Faz', PetsName:'Kitty'}, {user:'Rhiannon', PetsName:'Rosie'}}
+
+    # Your code for fetching the data goes here
+
     # Renders the html and can insert the data with a second argument 'data='
-    return render_template("index.html", data=data)
+    return render_template("index.html")
+
+
+@app.route("/pets")
+def pets():
+    # Example of data being rendered in html
+    data_list = [{"user": "Alex", "pets_name": "Fish"}, {
+        "user": "Faz", "pets_name": "Kitty"}, {"user": "Rhiannon", "pets_name": "Rosie"}]
+
+    return render_template("petsExample.html", data=data_list)
+
+
 if __name__ == "__main__":
     app.run()
