ff51045e77f6c5db20e03316b298fe1d0ca08d75
schofield-r <54988457+schofield-r@users.noreply.github.com>
2023-03-15 11:56:03 +0000

    test

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 @app.route("/")
 def index():
   # Your code for fetching the data goes here
-  
+    data={{user:'Alex', PetsName:'Fish'}, {user:'Faz', PetsName:'Kitty'}, {user:'Rhiannon', PetsName:'Rosie'}}
     # Renders the html and can insert the data with a second argument 'data='
-    return render_template("index.html")
+    return render_template("index.html", data=data)
 if __name__ == "__main__":
     app.run()
