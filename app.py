c17cdf0772d1a1818638c23d9bcea185bb97f6ea
Rhiannon Schofield <yenschofield@gmail.com>
2023-03-15 15:58:00 +0000

    add filter route and dropdown

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-import json
-from flask import Flask, render_template, send_file
+import requests
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -9,9 +9,10 @@ app = Flask(__name__)
 def index():
 
     # Your code for fetching the data goes here
-
+    response = requests.get('https://manchester-airport-flights.netlify.app')
+    data = response.json()
     # Renders the html and can insert the data with a second argument 'data='
-    return render_template("index.html")
+    return render_template("index.html", data=data)
 
 
 @app.route("/pets")
@@ -22,6 +23,22 @@ def pets():
 
     return render_template("petsExample.html", data=data_list)
 
+
+@app.route("/filter/<airline>")
+def filtered(airline):
+    print()
+    # Example of data being rendered in html
+    response = requests.get('https://manchester-airport-flights.netlify.app')
+    data = response.json()
+
+    filtered_data = []
+
+    for i in data:
+        if i['Airline'] == airline:
+            filtered_data.append(i)
+
+
+    return render_template("index.html", data=filtered_data)
 
 if __name__ == "__main__":
     app.run()
