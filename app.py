af6431651842f6a3284f970cdde066d94ce68f0f
schofield-r <54988457+schofield-r@users.noreply.github.com>
2023-04-18 11:36:24 +0100

    Update app.py
    temp adding request to test on devices

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-
+    
+    response = requests.get('https://manchester-airport-flights.netlify.app')
+    data = response.json()
     # Your code for fetching the API data goes here
     return render_template("index.html", data=data)
 
